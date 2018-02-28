b58e085481d2605b0c5ab7b4c7c16c8926761da6
CorlyBoy <gabicorlan82@gmail.com>
2018-02-28 20:24:54 +0200

    ClassSnake

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -9,8 +9,8 @@ namespace Snake
     public class Snake
     {
         public int Length;
-        public Point Head;
-        public Point Body = new List<Point>();
+        //public Point Head;
+        //public Point Body = new List<Point>();
 
         public Snake()
         {
@@ -19,22 +19,22 @@ namespace Snake
 
         public void MoveForward()
         {
-            
+            throw new NotImplementedException();
         }
 
         public void TurnLeft()
         {
-
+            throw new NotImplementedException();
         }
 
         public void TurnRight()
         {
-
+            throw new NotImplementedException();
         }
 
         public void Eat()
         {
-
+            throw new NotImplementedException();
         }
     }
 }
