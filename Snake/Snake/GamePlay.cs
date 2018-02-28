407163d50fd57eed0786013cbb109c262fa78661
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-02-28 20:21:32 +0200

    abstract class GamePlay

---
+++ Snake/Snake/GamePlay.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public abstract class GamePlay
+    {
+        public DateTime Speed;
+        public int GrowLength;
+        public string Wall;
+
+        public GamePlay(DateTime speed, string wall)
+        {
+            Speed = speed;
+            Wall = wall;
+        }
+
+        public abstract void Move();
+        public abstract void Controls();
+        public abstract void StartGame();
+        public abstract void EndGame();   
+    }
+}
