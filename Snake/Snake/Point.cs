b91d40960bca8fa4ebb7fc9399eadc4e95ff07ec
Dana <danaciocoiu78@gmail.com>
2018-02-28 20:27:49 +0200

    Public

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class Point
+    public class Point
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
