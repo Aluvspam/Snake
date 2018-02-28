7a12f28042f37e7e10c7b20fb6b5e197bd4b754c
oana.buturas <oana.buturas@gmail.com>
2018-02-28 23:36:11 +0200

    initial Food class

---
+++ Snake/Snake/Food.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Food
+    {
+
+        public Point Location;
+        public bool HealthyFood;
+
+        public void ResetLocation()
+        {
+            throw new NotSupportedException();
+        }
+
+
+
+    }
+
+
+}
