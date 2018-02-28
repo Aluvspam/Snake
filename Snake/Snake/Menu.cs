d2c79a7abaea577ab17ae121e453734b3061df6e
Cristian <cristian_radu_89@yahoo.com>
2018-02-28 20:18:09 +0200

    am adaugat clasele necesare meniului si am populat clasa Menu cu acestea

--- Snake/Snake/Menu.cs
+++ Snake/Snake/Menu.cs
@@ -8,5 +8,10 @@ namespace Snake
 {
     class Menu
     {
+        public Resume Resume;
+        public Restart Restart;
+        public Options Options;
+        public Highscore Highscore;
+        public Exit Exit;
     }
 }
