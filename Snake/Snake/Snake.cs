2dce43f31ae8f480f5e7cf19eb9099abb8c38b19
CorlyBoy <gabicorlan82@gmail.com>
2018-02-28 20:19:41 +0200

    firstCommit

---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class Snake
+    {
+        public int Length;
+        public Point Head;
+        public Point Body = new List<Point>();
+
+        public Snake()
+        {
+            Length = 3;
+        }
+
+        public void MoveForward()
+        {
+            
+        }
+
+        public void TurnLeft()
+        {
+
+        }
+
+        public void TurnRight()
+        {
+
+        }
+
+        public void Eat()
+        {
+
+        }
+    }
+}
