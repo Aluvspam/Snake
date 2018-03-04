33543b10e027a5cf2e1400a3b4a97a42a8fa950d
KalamityLu <luana.barbulescu@gmail.com>
2018-03-04 19:19:56 +0200

    Player_Class

---
+++ Snake/Snake/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	public class Player
+	{
+		public string playerName;
+		public int highScore;
+		public DateTime highScoreDate;
+	}
+}
