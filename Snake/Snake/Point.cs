e6c320a1051754440695b1d2d6a4fb1e906eebdc
Dana <danaciocoiu78@gmail.com>
2018-03-07 17:19:36 +0200

    Completari in  Point

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -8,18 +8,29 @@ namespace Snake
 {
     public class Point
     {
-        public int X { get; set; }
+        public int Width { get; set; } //ajutor in setarea obiectului sarpe, pozitie mancare, tabla de joc
+        public int Height { get; set; }
+        public bool EndGame { get; set; }// cat timp este fals jocul va continua
+        public int X { get; set; }//locatia x si y a obiectului snake
         public int Y { get; set; }
-
 
         public Point(int x, int y)
         {
+            Width = 0;//de setat valorile
+            Height = 0;
+            EndGame = false;
             X = x;
             Y = y;
         }
+
         public override string ToString()
         {
             return "(" + X + ", " + Y + ")";
         }
+        public void Die()
+        {
+            EndGame = true;
+        }
+
     }
 }
