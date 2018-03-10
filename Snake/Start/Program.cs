8f4fb274a653f6f28b170c5c07b856efd7a1b6e4
andrei patrascu <andreipatrascu@outlook.com>
2018-03-10 10:21:36 +0200

    linked Form1 from Dana to be started in Main method

--- Snake/Start/Program.cs
+++ Snake/Start/Program.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Start
 {
-    public class Program
+    static class Program
     {
-       public static void Main()
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
-            
-
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
